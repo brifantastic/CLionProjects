e9a9c2bb7174fa018960cf70456c67880b462421
Edward Brash <brash@jlab.org>
2023-09-19 08:56:38 -0400

    FileReader Updates

--- cpsc256/Examples/Week5/PointerCar/main.c
+++ cpsc256/Examples/Week5/PointerCar/main.c
@@ -17,10 +17,21 @@
 #include "PointerCar.h"
 
 /*
- * 
+ * This is the main program for the PointerCar example
+ *
+ * In comparison to the SimpleCar example, we will now
+ * use pointers to structs, rather than the structs themselves.
+ *
+ * This will allow us to manipulate the structs directly, rather
+ * than making copies of the structs.  This decreases the amount
+ * of memory used, and also allows us to update the structs
+ * directly.
+ *
  */
 int main(int argc, char** argv) {
-    
+
+    // Get information from the user - number of miles to drive forward,
+    // and the number of miles to drive in reverse.
    int milesForward, milesReverse;
    printf("Miles forward = ");
    scanf("%d", &milesForward);
@@ -29,30 +40,41 @@ int main(int argc, char** argv) {
    scanf("%d", &milesReverse);
    printf("\n");
 
+    // Initialize a new PointerCar struct, called myCar
    PointerCar myCar = InitCar();
+
+    // We will now create a pointer to the myCar struct, and use that
+    // pointer to manipulate the struct directly.
    PointerCar* pCar = &myCar;
-   
+
+   // We will now use the pointer to the struct to access the struct
+   // directly, and print out the address of the struct, and the
+   // mileage of the car (initially - should be zero!)
    printf("\n");
    printf("In main 1:  address of myCar = %p\n", pCar);
    printf("Mileage = %d\n", GetOdometer(pCar));
    printf("\n");
 
+    // Drive forward by a specified amount
    Drive(milesForward, pCar);
    printf("\n");
    printf("In main 2:  address of myCar = %p\n", pCar);
    printf("Mileage = %d\n", GetOdometer(pCar));
    printf("\n");
 
+    // Drive in reverse by a specified amount
    Reverse(milesReverse, pCar);
    printf("\n");
    printf("In main 3:  address of myCar = %p\n", pCar);
    printf("Mileage = %d\n", GetOdometer(pCar));
    printf("\n");
 
+    // Change the make, model, and age of the car
    SetMake(pCar, "Mercedes-Benz");
    SetModel(pCar, "E350");
    SetAge(pCar, 16.0);
-   
+
+    // Call the HonkHorn and Report methods
    HonkHorn(pCar);   
    Report(pCar);
 
