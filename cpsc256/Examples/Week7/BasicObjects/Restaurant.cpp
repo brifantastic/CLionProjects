e6e526a62e90864fb4f5325994b851138051b8e9
Edward Brash <brash@jlab.org>
2023-10-03 10:07:23 -0400

    FileReader Updates

--- cpsc256/Examples/Week7/BasicObjects/Restaurant.cpp
+++ cpsc256/Examples/Week7/BasicObjects/Restaurant.cpp
@@ -1,5 +1,10 @@
 #include "Restaurant.h"
 
+// Default constructor:
+// 1.  Initialize all private variables to some default value.
+// 2.  In the case of the member variable id, initialize it to the static member variable nextID.
+// 3.  Increment nextID so that the next object created will have a different id.
+
 Restaurant::Restaurant() {
     name = "No Name";
     rating = -1;
@@ -8,6 +13,11 @@ Restaurant::Restaurant() {
     id = nextID;
     ++nextID;
 }
+
+// Initialization constructor:
+// 1.  Initialize all private variables to the values passed in as parameters.
+// 2.  In the case of the member variable id, initialize it to the static member variable nextID.
+// 3.  Increment nextID so that the next object created will have a different id.
 
 Restaurant::Restaurant(string userName, int userRating, string price, string cuisineType) {
     name = userName;
@@ -27,7 +37,16 @@ Restaurant::Restaurant(string userName, int userRating, string price, string cui
     ++nextID;
 }
 
-int Restaurant::nextID = 1001; // Initialize nextID = the id number of the FIRST object created.
+// Static member variables are defined outside of the class definition!!!
+// They are initialized to some value, and then can be used by all objects
+// of the class.  In this case, we are using it to assign unique id values
+// to each object of the class.
+
+int Restaurant::nextID = 1001;
+
+// Mutator functions ... used to change the values of private member variables.
+// Note that we do not need to use the keyword "this" in this case, because
+// the parameter names are different from the member variable names.
 
 void Restaurant::SetName(string restaurantName) {
     name = restaurantName;
@@ -49,6 +68,12 @@ void Restaurant::SetCuisineType(string cuisineType) {
     return;
 }
 
+// Accessor functions ... used to return the values of private member variables.
+// In this example, we do not have any of these, because they are all
+// inline functions defined in the class definition (in the header file).
+
+
+// Print function ... used to print the values of private member variables.
 void Restaurant::Print() const {
     cout << name << ": " << "ID = " << id << endl;
     cout << "Rating = " << rating << " Price = " << price << " Cuisine Type = " 
