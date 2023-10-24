deba5b8c95746a07bbffdaaa68c214db142ba0a4
Edward Brash <brash@jlab.org>
2023-10-24 12:49:18 -0400

    FileReader Updates

--- cpsc256/Examples/Week11/LinkListDestructor/main.cpp
+++ cpsc256/Examples/Week11/LinkListDestructor/main.cpp
@@ -20,17 +20,26 @@ using namespace std;
  */
 int main(int argc, char** argv) {
     
-   // Create a linked list
+   // Create a linked list - this will just create a single node, the head,
+   // with data = 0
    LinkedList* list = new LinkedList;
    
-   // Prepend several nodes to list 
+   // Prepend several nodes to list
+   // After this loop, there will be 10 nodes in the list
+   // The head node will be the last node added (i.e., data = 100)
+   // The last node added will be the first node in the list (i.e., data = 10),
+   // and more importantly, this last node will have a null pointer for its next
+   // pointer, indicating that it is the last node in the list.
+
    for (int i = 1; i <= 10; ++i) {
       list->Prepend(i * 10);
       cout << "Head now at: " << list->GetHead() << endl;
    } 
       
    // Free the linked list.
-   // The LinkedList class destructor frees each node.
+   // The LinkedList class destructor frees each node, starting with the head node.
+   // The destructor will call the destructor for each node, which will free the
+   // next node, and so on, until the last node is freed.
    delete list;
 
    return 0;
