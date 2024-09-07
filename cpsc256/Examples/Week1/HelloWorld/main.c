78e7f21057af012377ea5fec01b19215f96379fb
Edward Brash <brash@jlab.org>
2024-09-07 10:38:44 -0400

    Updates

--- cpsc256/Examples/Week1/HelloWorld/main.c
+++ cpsc256/Examples/Week1/HelloWorld/main.c
@@ -19,9 +19,9 @@
 /************************************************************/
 
 int main(int argc, char** argv) {
-    
+
     printf("Hello World!!!!\n");
-    
     return (EXIT_SUCCESS);
+
 }
 
